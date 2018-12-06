6c838b4ef000b5bce9c76bcaf31cd09376342d35
Nawa Manusitthipol <nawa@nawaman.net>
2018-12-06 10:31:35 -0500

    Improve default method invocation to work with Java 8-9-10

--- nawaman-utils-reflection/src/main/java/nawaman/utils/reflection/UProxy.java
+++ nawaman-utils-reflection/src/main/java/nawaman/utils/reflection/UProxy.java
@@ -17,6 +17,9 @@ package nawaman.utils.reflection;
 import static java.util.Arrays.asList;
 
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
@@ -110,17 +113,46 @@ public class UProxy {
     public static Object invokeDefaultMethod(@NonNull Object proxy, @NonNull Method method, Object[] methodArgs) 
                     throws NotDefaultMethodException, Throwable {
         // TODO - See if we can avoid this in case it is already done.
-        val defaultMethod = getDefaultMethod(method);
+        val defaultMethod  = getDefaultMethod(method);
         val declaringClass = defaultMethod.getDeclaringClass();
-        val constructor    = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
-        constructor.setAccessible(true);
-        
-        val result = constructor
-                .newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
-                .unreflectSpecial(defaultMethod, declaringClass)
+        val lookup         = getLookup(declaringClass);
+        return invoke(lookup, declaringClass, method, proxy, methodArgs);
+    }
+    
+    // Thanks to https://blog.jooq.org/2018/03/28/correct-reflective-access-to-interface-default-methods-in-java-8-9-10
+    private static Object invoke(Lookup lookup, Class<?> type, Method method, Object proxy, Object[] methodArgs)
+            throws IllegalAccessException, Throwable {
+        val mthdName       = method.getName();
+        val mthdReturnType = method.getReturnType();
+        val mthdParamTypes = method.getParameterTypes();
+        val mthdType       = MethodType.methodType(mthdReturnType, mthdParamTypes);
+        val result = lookup
+                .findSpecial(type, mthdName, mthdType, type)
                 .bindTo(proxy)
                 .invokeWithArguments(methodArgs);
         return result;
+    }
+    
+    private static Method getPrivateLookupInMethod() throws NoSuchMethodException {
+        try {
+            return MethodHandles.class.getMethod("privateLookupIn", new Class[] { Class.class, Lookup.class });
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
+    }
+    
+    private static Lookup getLookup(Class<?> type) throws Exception {
+        val rawLookup = MethodHandles.lookup();
+        val method = getPrivateLookupInMethod();
+        if (method != null) {
+            val lookup = (Lookup)method.invoke(MethodHandles.class, type, rawLookup);
+            return lookup;
+        } else {
+            Constructor<Lookup> constructor = Lookup.class.getDeclaredConstructor(Class.class);
+            constructor.setAccessible(true);
+            val lookup = constructor.newInstance(type);
+            return lookup;
+        }
     }
     
     private static Method getDefaultMethod(Method method) {
